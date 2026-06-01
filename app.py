9cba7e4c31c9ba1174c24dabf23da25038d40fa3
pinturajput0777 <pinturajput0777@gmail.com>
2026-06-01 14:33:48 +0530

    Fix: Bypassed Werkzeug production server environment restriction flag

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get('PORT', 10000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
